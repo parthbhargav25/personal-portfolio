deb263536fb7da1f240144efc1a740c8fcd55028
Parth Bhargav <parthbhargav123@gmail.com>
2023-08-04 00:35:54 +0530

    header updadted

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Particles from  "react-particles-js";
+import Particles from "react-particles-js";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
-       <Particles
+      <Particles
         className="particles-canvas"
         params={{
           particles: {
@@ -26,7 +26,7 @@ function App() {
               }
             }
           }
-        }} 
+        }}
       />
       <Navbar />
       <Header />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-dark">
       <div className="container">
 
-      <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo..." /></a>
+        <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo..." /></a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }} />
         </button>
