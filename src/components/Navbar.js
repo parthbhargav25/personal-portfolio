b6adba0407b0a882556c9ce2cdd98f8bfab69bd0
Parth Bhargav <parthbhargav123@gmail.com>
2023-08-04 21:35:17 +0530

    About me completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
-import logo from "../logo.png";
+//import Mylogo from "../Mylogo.jpg";
+
 // REACT FONTAWESOME IMPORTS
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
@@ -9,7 +10,7 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-dark">
       <div className="container">
 
-        <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo..." /></a>
+        <a className="navbar-brand" href="#">Parth Bhargav</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }} />
         </button>
