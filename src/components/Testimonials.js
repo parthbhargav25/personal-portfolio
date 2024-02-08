099864fec4a35a2a89fa7061e146b24842322e9e
Parth Bhargav <parthbhargav123@gmail.com>
2024-02-08 12:35:07 +0530

    projects updated

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -4,7 +4,7 @@ import TestimonialsCarousel from "./TestimonialsCarousel";
 const Testimonials = () => {
   return (
     <div id="testimonials" className="testimonials">
-      <h1>my happy clients</h1>
+      <h1>References</h1>
       <div className="container">
         <div className="testimonials-content">
           <TestimonialsCarousel />
